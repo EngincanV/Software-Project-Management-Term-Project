63beee47f93b676170316b6870b745b04552f273
Osman Yavuz Demir <Osman Yavuz Demir>
2020-04-23 01:37:39 +0300

    Header componenti tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Route, Switch, BrowserRouter as Router, Redirect} from 'react-router-dom'
 import './App.css';
-
+import "antd/dist/antd.css";
+import HeaderComponent from './components/Header'
 import Main from './pages/main/Main'
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Router>
+        <HeaderComponent />
         <Switch>
           <Route exact path="/" component={Main} />
         </Switch>
