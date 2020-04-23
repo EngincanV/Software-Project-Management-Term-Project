6c6cfe894722a17065e456968762361657e20dbb
Engincan Veske <enginveske@gmail.com>
2020-04-23 21:09:10 +0300

    rock-paper-scissors game has been added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Menu,  Row, Col } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { Header, Content, Sider } = Layout;
 
@@ -11,7 +12,7 @@ function HeaderComponent(){
                     <Col span={10}> <div className="logo">Machine Learning for Kids</div></Col>
                     <Col span={14} style={{textAlign:"right"}}>
                         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                            <Menu.Item key="1">Anasayfa</Menu.Item>
+                            <Menu.Item key="1"><Link to="/">Anasayfa</Link></Menu.Item>
                             <Menu.Item key="2">Acemi</Menu.Item>
                             <Menu.Item key="3">Orta Düzey</Menu.Item>
                             <Menu.Item key="3">İleri Seviye</Menu.Item>
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,80 +1,82 @@
 import React from 'react'
 import { Card, Tag, Button, Layout } from 'antd';
+import { Link } from 'react-router-dom';
 
-const { Content} = Layout;
-function Main(){
-    return(
-        <div style={{ padding:"50px"}}>
+const { Content } = Layout;
+function Main() {
+    return (
+        <div style={{ padding: "50px" }}>
             <Content className="gameBox">
-                <Card className="game" style={{backgroundColor:"purple"}} >
+                <Card className="game" style={{ backgroundColor: "purple" }} >
                     <div className="title">Smart Classroom</div>
                     <p>Create a smart assistant in Scratch that lets you control virtual devices. Teach a computer to recognise the meaning of your commands</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="orange">Emülatör</Tag> <Tag color="cyan"> Acemi </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="orange">Emülatör</Tag> <Tag color="cyan"> Acemi </Tag></div>
                 </Card>
-                <Card className="game" style={{backgroundColor:"blue"}} >
+                <Card className="game" style={{ backgroundColor: "blue" }} >
                     <div className="title">Make me happy</div>
                     <p>Create a character in Scratch that smiles if you say nice things to it and cries if you say mean things to it. Teach a computer to recognise compliments and insults</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"red"}} >
+                <Card className="game" style={{ backgroundColor: "red" }} >
                     <div className="title">Snap!</div>
                     <p>Make a card game in Scratch that learns to recognise pictures of your card. Teach a computer to recognise what icons look like</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
                 </Card>
-                <Card className="game" style={{backgroundColor:"green"}} >
+                <Card className="game" style={{ backgroundColor: "green" }} >
                     <div className="title">Chameleon</div>
                     <p>Make a chameleon in Scratch that changes colour to match its background. Teach a computer to recognise colours</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="red"> İleri Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="red"> İleri Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"orange"}} >
+                <Card className="game" style={{ backgroundColor: "orange" }} >
                     <div className="title">Titanic</div>
                     <p>Create a Python program that can predict who survived the sinking of the Titanic. Teach a computer to predict outcomes</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="orange"> Emülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="orange"> Emülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"Brown"}} >
+                <Card className="game" style={{ backgroundColor: "Brown" }} >
                     <div className="title">Mailman Max </div>
                     <p>Make a postal sorting office in Scratch that can recognise handwritten postcodes on envelopes. Teach a computer to recognise handwriting</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"purple"}} >
+                <Card className="game" style={{ backgroundColor: "purple" }} >
                     <div className="title">Car or cup</div>
                     <p>Train the computer to be able to sort photos into groups. Teach a computer to recognise pictures of objects</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Acemi </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Acemi </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"blue"}} >
+                <Card className="game" style={{ backgroundColor: "blue" }} >
                     <div className="title">Face Lock</div>
                     <p>Make a phone in Scratch that unlocks if it recognises your face. Teach a computer to recognise faces</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="orange"> Emülatör</Tag> <Tag color="red"> İleri Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="orange"> Emülatör</Tag> <Tag color="red"> İleri Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"red"}} >
+                <Card className="game" style={{ backgroundColor: "red" }} >
                     <div className="title">Journey to school</div>
                     <p>Train the computer to be able to predict how you travel to school in the morning. Teach a computer to make predictions</p>
                     <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
 
                 </Card>
-                <Card className="game" style={{backgroundColor:"green"}} >
-                    <div className="title">Shy Panda</div>
-                    <p>Make a dancing panda that gets shy and stops dancing if it sees you looking. Teach a computer to recognise webcam pictures</p>
-                    <Button ghost> Start Game</Button>
-                    <div style={{marginTop:"20px"}}> <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Orta Düzey </Tag></div>
+                <Card className="game" style={{ backgroundColor: "green" }} >
+                    <div className="title">Rock Paper Scissors</div>
+                    <p>Make a Rock, Paper, Scissors game in Scratch that learns to recognise hand shapes.
+                    Teach a computer to recognise shapes</p>
+                    <Button ghost><Link to="rock-paper-scissors">Start Game</Link></Button>
+                    <div style={{ marginTop: "20px" }}> <Tag color="pink"> Emülatör</Tag> <Tag color="green"> Acemi </Tag></div>
 
                 </Card>
-            
+
             </Content>
         </div>
     )
