6dae07b1ee2360670597f8015f94800c3d5c9263
Engincan Veske <enginveske@gmail.com>
2020-05-19 16:14:56 +0300

    last changes

--- src/pages/games/Titanic.js
+++ src/pages/games/Titanic.js
@@ -6,9 +6,6 @@ import titanic from "../../assets/titanic/1.jpg";
 function Titanic(){
     const [TClass, setTClass] = useState("")
     const [TAge, setTAge] = useState("")
-    const [TCount, setTCount] = useState("")
-    const [TParent, setTParent] = useState("")
-    const [TCost, setTCost] = useState("")
 
     const deadorAlive = (data) => {
         return axios.post('https://machinelearningdeploy.herokuapp.com/predict',  data )
@@ -21,7 +18,7 @@ function Titanic(){
        });
     }
     const handleClick = () => {
-        if(TClass !== "" && TAge !== "" && TCount !== "" && TParent !== "" && TCost !== "") {
+        if(TClass !== "" && TAge !== "") {
             var bodyFormData = new FormData();
             bodyFormData.set('Age', TAge);
             deadorAlive(bodyFormData).then(result => {
@@ -50,16 +47,18 @@ function Titanic(){
             <h1>Aşağıdaki bilgileri doldurun ve yolcunun titanik kazasında hayatta kalıp kalmayacağını kontrol edin.</h1>
             <div> <img src={titanic} /></div>
             <div style={{marginTop:25}}>
-                <input placeholder="Bilet Sınıfı" style={{marginRight:10}} value={TClass} onChange={e => setTClass(e.target.value)}/>
+                <select onChange={e => setTClass(e.target.value)} name="" id="" style={{marginRight:10}}>
+                    <option disabled>Lütfen yolcunun bilet sınıfını seçiniz</option>
+                    <option>Birinci Sınıf</option>
+                    <option>İkinci Sınıf</option>
+                    <option>Üçüncü Sınıf</option>
+                </select>
                 <select style={{marginRight:10}} >
-                    <option> Lütfen Yolcunun Cinsiyetini Seçiniz</option>
+                    <option disabled> Lütfen Yolcunun Cinsiyetini Seçiniz</option>
                     <option> Kadın </option>
                     <option> Erkek </option>
                 </select>
-                <input placeholder="Yolcu Yaşı" style={{marginRight:10}}  value={TAge} onChange={e => setTAge(e.target.value)}/>
-                <input placeholder="Yolculuğa Kaç Kişi Katıldı" style={{marginRight:10}} value={TCount} onChange={e => setTCount(e.target.value)}/>
-                <input placeholder="Yolcunun Ebeveyn yada çocuk sayısı" style={{marginRight:10}} value={TParent} onChange={e => setTParent(e.target.value)}/>
-                <input placeholder="Bilet Ücreti" style={{marginRight:10}} value={TCost} onChange={e => setTCost(e.target.value)}/>
+                <input type="number" placeholder="Yolcu Yaşı" style={{marginRight:10}}  value={TAge} onChange={e => setTAge(e.target.value)}/>
                 <select style={{marginRight:10}} >
                     <option> Lütfen Yolcunun Biniş Yerini Seçiniz</option>
                     <option> Cherbourg </option>
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -15,89 +15,11 @@ function Main() {
             .catch(err => console.log("err"))
     }, []);
 
-    const gameList = () => {
-        // eslint-disable-next-line no-unused-expressions
-        list ? list.map(game => {
-            return (
-                <Card className="game" style={{ backgroundColor: backgroundColors[Math.floor(Math.random() * backgroundColors.length)] }}>
-                    <div className="title">{game.GameName}</div>
-                    <p>
-                        {game.GameDescription}
-                    </p>
-
-                    <Button ghost> Start Game</Button>
-                    <div style={{ marginTop: '20px' }}>
-                        {' '}
-                        <Tag color="pink"> Simülatör</Tag>{' '}
-                        <Tag color="green"> Orta Düzey </Tag>
-                    </div>
-                </Card>
-                <Card className="game" style={{ backgroundColor: 'green' }}>
-                    <div className="title">Chameleon</div>
-                    <p>
-                        Make a chameleon in Scratch that changes colour to match its
-                        background. Teach a computer to recognise colours
-                     </p>
-                     <Button chameleon>
-                        <Link to="chameleon">Start Game</Link>
-                    </Button>
-                    <div style={{ marginTop: '20px' }}>
-                        {' '}
-                        <Tag color="pink"> Simülatör</Tag>{' '}
-                        <Tag color="red"> İleri Düzey </Tag>
-                    </div>
-                </Card>
-                <Card className="game" style={{ backgroundColor: 'orange' }}>
-                    <div className="title">Titanic</div>
-                    <p>
-                        Create a Python program that can predict who survived the sinking of
-                        the Titanic. Teach a computer to predict outcomes
-          </p>
-                    <Button ghost> Start Game</Button>
-                    <div style={{ marginTop: '20px' }}>
-                        {' '}
-                        <Tag color="orange"> Emülatör</Tag>{' '}
-                        <Tag color="green"> Orta Düzey </Tag>
-                    </div>
-                </Card>
-                <Card className="game" style={{ backgroundColor: 'purple' }}>
-                    <div className="title">Sorting Hat</div>
-                    <p>
-                        Create a Sorting Hat like in Harry Potter, that puts you in a school
-                        House based on what you say.
-          </p>
-                    <Button ghost>
-                        <Link to="sorting-hat">Start Game</Link>
-                    </Button>
-                    <div style={{ marginTop: '20px' }}>
-                        {' '}
-                        <Tag color="pink"> Simülatör</Tag> <Tag color="green"> Acemi </Tag>
-                    </div>
-                </Card>
-                <Card className="game" style={{ backgroundColor: 'blue' }}>
-                    <div className="title">Chatbots</div>
-                    <p>
-                        Create a chatbot that can answer questions about a topic of your
-                        choice.
-          </p>
-
-                    <Button ghost>
-                        <Link to={game.GameName.toString().replace(" ", "-")}>Start Game</Link>
-                    </Button>
-                    <div style={{ marginTop: '20px' }}>
-                        {' '}
-                        <Tag color={game.GameType === "Simülatör" ? "pink" : "orange"}>{game.GameType}</Tag> <Tag color={game.GameLevel === "Acemi" ? "cyan" : game.GameLevel === "Orta Düzey" ? "green" : "red"}> {game.GameLevel} </Tag>
-                    </div>
-                </Card>
-            )
-        }) : <p>Loading ...</p>
-    }
     return (
         <div style={{ padding: '50px' }}>
             <Content className="gameBox">
                 {
                     list.length >= 1 ? list.map(game => {
-                        console.log(game.GameName.toString().toLowerCase().split)
                         return (
                             <Card className="game" style={{ backgroundColor: backgroundColors[Math.floor(Math.random() * backgroundColors.length)] }}>
                                 <div className="title">{game.GameName}</div>
